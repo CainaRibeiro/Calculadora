a16d7d4edbe3556290a14c62fa104c939c4618ac
CainaRibeiro <cainaribeiro.it@outlook.com>
2022-06-16 23:02:13 -0300

    -

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
+var ca    = document.getElementById('ca');
+var res   = document.getElementById('res');
+var mais  = document.getElementById('mais')
+var somar = " "
 var n1 = 1;
 var n2 = 2;
 var n3 = 3;
-var mais = mais;
+var n4 = 4;
+var n5 = 5;
+var n6 = 6;
+var n7 = 7;
+var n8 = 8;
+var n9 = 9;
 
+
+function ca(){
+    res.innerHTML = `Resultado`
+}
 
 function somar() {
+    res.innerHTML += `+`
     
 }
+function um() {
+    res.innerHTML += `1`
+}
+function dois() {
+    res.innerHTML += `2`
+}
+function tres() {
+    res.innerHTML += `3`
+}
+function quatro() {
+    res.innerHTML += `4`
+}
+function cinco() {
+    res.innerHTML += `5`
+}
+function seis() {
+    res.innerHTML += `6`
+}
+function sete() {
+    res.innerHTML += `7`
+}
+function oito() {
+    res.innerHTML += `8`
+}
+function nove() {
+    res.innerHTML += `9`
+}
+
+
+
